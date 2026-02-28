e1d3b8b4f51ad346c3702b3c9ee50b6c88f48c07
Chris Gaaju <chrisgaajukpeme2027@gmail.com>
2026-02-28 07:58:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,27 @@
+require('dotenv').config();
+
 const settings = {
-  prefixes: ['.', '!', '/', '#'], // Multiple prefix support you can add one or more
-  packname: 'GAAJU-MD',
-  author: '‎Xchristech2',
-  timeZone: 'africa/lagos',
-  botName: "GAAJU-MD", // your bot name
-  botOwner: 'Chris Gaaju', // set your name
-  ownerNumber: '2348069675806', // Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20,
-  tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
-  storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "5.1.0",
-  updateZipUrl: "https://github.com/Xchristech2/GAAJU-MD/archive/refs/heads/main.zip",
-  channelLink: "https://whatsapp.com/channel/0029VbBvGgyFsn0alyIDjw0z",
-  ytch: "Xchristech"
+  // Array fallback: splits string by comma, or uses default array
+  prefixes: process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.', '!', '/', '#'],
+  
+  packname: process.env.PACKNAME || 'GAAJU-MD',
+  author: process.env.AUTHOR || 'Xchristech2',
+  timeZone: process.env.TIMEZONE || 'Africa/Lagos',
+  botName: process.env.BOT_NAME || "GAAJU-MD",
+  botOwner: process.env.BOT_OWNER || 'Chris Gaaju',
+  ownerNumber: process.env.OWNER_NUMBER || '2348069675806',
+  giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: process.env.COMMAND_MODE || "public",
+  
+  maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 20,
+  tempCleanupInterval: Number(process.env.CLEANUP_INTERVAL) || 1 * 60 * 60 * 1000,
+  storeWriteInterval: Number(process.env.STORE_WRITE_INTERVAL) || 10000,
+  
+  description: process.env.DESCRIPTION || "This is a bot for managing group commands and automating tasks.",
+  version: "5.2.0",
+  updateZipUrl: process.env.UPDATE_URL || "https://github.com/Xchristech2/GAAJU-MD/archive/refs/heads/main.zip",
+  channelLink: process.env.CHANNEL_LINK || "https://whatsapp.com/channel/0029VbBvGgyFsn0alyIDjw0z",
+  ytch: process.env.YT_CHANNEL || "Xchristech"
 };
 
 module.exports = settings;
-
